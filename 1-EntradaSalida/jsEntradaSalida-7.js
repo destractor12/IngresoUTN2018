868deb77be3506bcf522ac420e83666f4175e5d8
destractor12 <36230522+destractor12@users.noreply.github.com>
2018-02-07 12:27:41 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,54 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var num1;
+    var num2;
+    var suma;
+
+        num1 = parseInt (document.getElementById("numeroUno").value);
+        num2 = parseInt (document.getElementById("numeroDos").value);
+
+            suma(num1+num2);
+
+alert("La suma es: " + suma);
 }
 
 function restar()
-{
-	
+{	
+	var num1;
+    var num2;
+    var resta;
+
+        num1 = parseInt(document.getElementById("numeroUno")).value;
+        num2 = parseInt(document.getElementById("numeroDos")).value;
+
+            resta(num1-num2);
+alert("La resta es: " +resta);            
 }
 
 function multiplicar()
 { 
-	
+	var num1;
+    var num2;
+    var mult;
+
+        num1 = parseInt(document.getElementById("numeroUno")).value;
+        num2 = parseInt(document.getElementById("numeroDos")).value;
+
+            mult(num1*num2);
+alert("La multiplicacion es: " +mult);
 }
 
 function dividir()
 {
-	
+	var num1;
+    var num2;
+    var div;
+
+        num1 = parseInt(document.getElementById("numeroUno")).value;
+        num2 = parseInt(document.getElementById("numeroDos")).value;
+
+            div(num1/num2);
+alert("La division es: " +div);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,13 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var num1;
+    var num2;
+    var div;
+
+        num1 = parseInt(document.getElementById("numeroUno")).value;
+        num2 = parseInt(document.getElementById("numeroDos")).value;
+
+            div(num1/num2);
+alert("La division es: " +div);
 }
