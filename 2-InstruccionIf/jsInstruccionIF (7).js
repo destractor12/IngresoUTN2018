0b674889badad5bdf6995a93a264c484da0ac97f
destractor12 <36230522+destractor12@users.noreply.github.com>
2018-02-10 13:07:38 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -8,9 +8,23 @@ var soltero
     edad = parseInt(document.getElementById("edad").value);
     estadocivil = document.getElementById("estadoCivil").value;
     
-    if(edad<=18 && estadocivil != soltero)
+    if(edad<18 && estadocivil == soltero)
+    {
+        alert("Esta bien");
+    }
+    
+    else if(edad<18 && estadocivil != soltero)
     {
         alert("Es muy pequeño para NO ser soltero.");
     }
+    else if(edad<18 && estadocivil != soltero)
+    {
+        alert("Es muy pequeño para NO ser soltero.")
+    }
+    else 
+    {
+        alert("Esta bien.");
+    }
+  
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
+    var edad 
+    var estadocivil
+    var soltero
+    
+    edad= parseInt(document.getElementById("edad").value);
+    estadocivil= document.getElementById("estadoCivil").value;
 
-	
-
-
+    if(edad>=18 && estadocivil == soltero)
+    {
+        alert("Es soltero y no es menor.");
+    }
 }//FIN DE LA FUNCIÓN
