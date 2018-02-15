e8859a42a276850a3bd97ac9172a3c81af5b5480
destractor12 <36230522+destractor12@users.noreply.github.com>
2018-02-15 11:50:20 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,11 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+    while(clave != "utn750")
+    {
+        clave = prompt("clave erronea. intente nuevamente")
+
+    }
+alert("IZI")
 
 }//FIN DE LA FUNCIÓN
