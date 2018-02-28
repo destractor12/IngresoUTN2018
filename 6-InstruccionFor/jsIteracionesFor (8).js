d829789e4afb90dc309a1a84e3f2b2bd181ee4e6
destractor12 <36230522+destractor12@users.noreply.github.com>
2018-02-28 10:27:02 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,32 @@
 function Mostrar()
 {
 
+    var num;
+    var esprimo = true;
+    
+    num = prompt("Ingrese un numero:")
+    
+            for(var i=2; i<num ;i++)
+            {   
+      
+                if(num%1 == 0)
+                {
+                    esprimo = false;
+                    break;
+                }
+            }
 
+        if(esprimo == true)
+        {
+           alert("El numero "+num +" es primo")   
+        }
+    
+    else
+    {
+
+    alert("El numero " + num+ " no es primo")
+
+    }
 
 
 }//FIN DE LA FUNCIÓN
