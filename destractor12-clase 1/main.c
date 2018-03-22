18724c29cb65367e9f72dda63c6c379c7be7723d
destractor12 <36230522+destractor12@users.noreply.github.com>
2018-03-22 10:36:39 -0300

    Add files via upload

---
+++ destractor12-clase 1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+/*main o es codigo orientado a objetos\t para tabular, \n para un salto de renglon y \a para hacer sonar el pitido del mother. todos van adentro de las comillas.
+int (%d) (entero) 5  9   -4
+float    (%f)   (numeros con coma)    3.25    -6.7    -23.464861
+char    (%c) (caracter) "a"     "5"     "{"
+la , concatena
+%.2f es para que solo haya 2 numeros despues de la coma
+*/
